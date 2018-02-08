6998804e35864c91a2166e0ae590771c310c7fe9
Brian <calleja.records@gmail.com>
2018-02-07 16:16:04 -0800

    Practica WeatherData

---
+++ 2018-02-07 (WeatherData)/Trabajo 1.4 (Loose Connection)/Display1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_1._4__Loose_Connection_
+{
+    class Display1 : Observer
+    {
+        float temperatura;
+        float humedad;
+        float presion;
+    
+        public void display()
+        {
+            float resultado = temperatura + humedad + presion;
+            Console.WriteLine("Display 1 On: " + resultado);
+        }
+
+        public void update(float temp, float humidity, float pressure)
+        {
+            this.temperatura = temp;
+            this.humedad = humidity;
+            this.presion = pressure;
+        }
+    }
+}
---
+++ 2018-02-07 (WeatherData)/Trabajo 1.4 (Loose Connection)/weatherData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_1._4__Loose_Connection_
+{
+    class weatherData : Subject
+    {
+        float temp { get; set; }
+        float humidity { get; set; }
+        float pressure { get; set; }
+
+        List<Observer> subscribers = new List<Observer>();
+
+        public void notifyObserver()
+        {
+            foreach (Observer clientes in subscribers)
+            {
+                clientes.update(temp,humidity,pressure);
+            }
+        }
+
+        public void registerObserver(Observer observer)
+        {
+            subscribers.Add(observer);
+        }
+
+        public void removeObserver(Observer observer)
+        {
+            subscribers.Remove(observer);
+        }
+        public void messurementChanged(float temperatura, float humedad, float presion) {
+            
+            this.temp = temperatura;
+            this.humidity = humedad;
+            this.pressure = presion;
+            notifyObserver();
+        }
+    
+
+    }
+}
