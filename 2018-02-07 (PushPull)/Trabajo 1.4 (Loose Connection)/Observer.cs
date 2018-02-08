04f35bcd7fcd14ad225b9607412f1a74b8f8034e
Brian <calleja.records@gmail.com>
2018-02-07 20:37:04 -0800

    Practica (WeatherData)

---
+++ 2018-02-07 (PushPull)/Trabajo 1.4 (Loose Connection)/Observer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_1._4__Loose_Connection_
+{
+    interface Observer
+    {
+        void update(float temp, float humidity, float pressure);
+        void display();
+    }
+}
---
+++ 2018-02-07 (PushPull)/Trabajo 1.4 (Loose Connection)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_1._4__Loose_Connection_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new weatherData();
+            float temp = 20;
+            float humidity = 20;
+            float pressure = 20;
+            api.messurementChanged(temp, humidity, pressure);
+            Observer display1 = new Display1();
+            Observer display2 = new Display2();
+            Observer display3 = new Display3();
+            api.registerObserver(display1);
+            api.registerObserver(display2);
+            api.registerObserver(display3);
+            api.removeObserver(display2);
+            api.notifyObserver();
+            display1.display();
+            display2.display();
+            display3.display();
+            Console.ReadLine();
+        }
+    }
+}
